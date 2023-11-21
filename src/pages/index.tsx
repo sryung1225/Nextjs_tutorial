31f213201e14a6441109c1c6328070fd538495aa
sryung <sryung1225@gmail.com>
2023-11-21 19:21:31 +0900

    [📚 : practice] 페이지 별 head title 적용하기
    - CRA의 React Helmet을 대신한 내장 기능 탑재

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import Seo from "@/components/Seo";
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1>안녕안녕!</h1>
     </div>
   );
