c80eead27c20e0d9abd43f721f4610a3eaf68c5a
sryung <sryung1225@gmail.com>
2023-11-23 22:58:10 +0900

    [📚 : practice] 이미지 삽입하기
    - public 폴더에 이미지 파일 두고 사용
    - <img /> 태그 이용해도 되지만 자체적으로 최적화를 적용해주는 next/image 사용을 권장하는 알림이 노출됨
    - next/image 사용시 width, height 작성 필수

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,36 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function NavBar() {
   const router = useRouter();
   return (
-    <nav className="flex justify-center items-center gap-2.5 py-5 nav-box-shadow">
-      <Link
-        href="/"
-        className={`font-medium text-lg ${
-          router.pathname === "/" ? "text-[tomato]" : ""
-        }`}
-      >
-        Home
-      </Link>
-      <Link
-        href="/about"
-        className={`font-medium text-lg ${
-          router.pathname === "/about" ? "text-[tomato]" : ""
-        }`}
-      >
-        About
-      </Link>
+    <nav className="flex flex-col items-center gap-2.5 pt-2.5 pb-5 nav-box-shadow">
+      <Image
+        src="/vercel.svg"
+        width={100}
+        height={20}
+        alt="logo"
+        className="mb-2.5"
+      />
+      <div className="flex gap-2.5">
+        <Link
+          href="/"
+          className={`font-medium text-lg ${
+            router.pathname === "/" ? "text-[tomato]" : ""
+          }`}
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={`font-medium text-lg ${
+            router.pathname === "/about" ? "text-[tomato]" : ""
+          }`}
+        >
+          About
+        </Link>
+      </div>
     </nav>
   );
 }
