91a6d95e82293b3ba4f93bf9bc4a2ee71e28dbf9
sryung <sryung1225@gmail.com>
2023-11-24 00:14:16 +0900

    [📚 : practice] 데이터 fetch하기
    - 임의로 API키 직접 생략 후 커밋 진행

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,46 @@
+import { useEffect, useState } from "react";
 import Seo from "@/components/Seo";
-import Head from "next/head";
+
+const API_KEY = "";
+
+interface IMovie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
 export default function Home() {
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>안녕안녕!</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
