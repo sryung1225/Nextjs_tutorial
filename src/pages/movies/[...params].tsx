336ff5514df71cc6930d0aef6a4f2cf41790a3ef
sryung <sryung1225@gmail.com>
2023-11-29 01:13:54 +0900

    [📚 : practice] getServerSideProps로 서버 사이드 렌더링하기
    - Detail 페이지에 영화 제목이 페이시 소스로도 보여지도록
    - getServerSideProps 함수에서 반환된 데이터를 사용해 각 request에서 페이지 pre-rendering을 함

--- src/pages/movies/[...params].tsx
+++ src/pages/movies/[...params].tsx
@@ -1,14 +1,23 @@
+import { InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
 
-type TMovieDetailParams = [string, number];
-
-export default function Detail() {
+export default function Detail({
+  params,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
-  const [title, id] = (router.query.params as TMovieDetailParams) || [];
+  const [title, id] = router.query.params || [];
   // console.log(router);
   return (
     <>
-      <h4 className="block py-10 text-center text-xl font-semibold">{title}</h4>
+      <h4>{title}</h4>
     </>
   );
+}
+
+export function getServerSideProps({ params: { params } }: any) {
+  return {
+    props: {
+      params,
+    },
+  };
 }
